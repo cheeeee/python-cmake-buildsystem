85aba8e8e452f0fc863a3a0c9c4806f07e56996c
Nikita Bozhenko <nbozhenko@mapr.com>
2020-05-17 01:07:54 -0700

    fixed openssl build script

--- scripts/openssl.sh
+++ scripts/openssl.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 
 cd ..
-git clone git@github.com:openssl/openssl.git
+git clone https://github.com/openssl/openssl.git
 cd openssl
-git checkout OpenSSL_1_1_1-stable
+git checkout OpenSSL_1_1_1g
 ./Configure linux-generic64 no-ssl2 no-ssl3 no-shared -static -static-libgcc --prefix=/opt/openssl
 make -j12
 sudo make install
